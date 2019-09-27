9873d1927555fded643ae327f048141ace2995b2
jon tannarong <jonnaro@gmail.com>
2019-09-28 00:56:16 +0700

    add form

---
+++ js/event_formatter.js
@@ -0,0 +1,8 @@
+// format firebase event string and output to page.
+
+function showInput() {
+
+    document.getElementById('event_output').innerHTML =
+      document.getElementById("textarea1").value;
+
+}
